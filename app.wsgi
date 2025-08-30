f2bb3495a2589bb8749307a1244520b75122f437
BlueTot <bluetotkc@gmail.com>
2025-08-30 17:22:45 +0100

    feat: added signup route functionality

--- app.wsgi
+++ app.wsgi
@@ -1,6 +1,10 @@
 import sys
 import os
+from dotenv import load_dotenv
 
 sys.path.insert(0, os.path.dirname(__file__))
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path)
 
 from backend.app import application
--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,8 @@ import mysql.connector
 import os
 import urllib.parse
 from contextlib import contextmanager
+import bcrypt
+import uuid
 
 WSGIEnvironment = dict[str, Any]
 StartResponse = Callable[[str, list[tuple[str, str]]], None]
@@ -25,6 +27,7 @@ def get_db() -> Iterator[MySQLConnection]:
     finally:
         conn.close()
 
+
 def get_params(environ: WSGIEnvironment) -> dict[str, str]:
     """
         Function to get the params from a POST request
@@ -32,6 +35,28 @@ def get_params(environ: WSGIEnvironment) -> dict[str, str]:
     return dict(urllib.parse.parse_qsl(
         environ['wsgi.input'].read(int(environ['CONTENT_LENGTH'])).decode())
     )
+
+
+def hash_password(password: str) -> str:
+    """
+        Function to hash a password string
+    """
+    encoded = password.encode("utf-8")
+    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
+    return hashed.decode()
+
+
+def redirect(start_response: StartResponse, location: str):
+    """
+        Function to redirect user to given route
+        Returns a response
+    """
+    start_response("302 Found", [
+        ("Location", location),
+        ("Content-Type", "text/plain")
+    ])
+    return [b"Redirecting..."]
+
 
 def application(
         environ: WSGIEnvironment, 
@@ -45,6 +70,8 @@ def application(
 
     if path == "/signup":
         return signup(environ, start_response)
+    elif path == "/login":
+        return login(environ, start_response)
 
     data = {"message": f"Hello from backend/app.py! You are visiting {path}"}
     response_body: bytes = json.dumps(data).encode("utf-8")
@@ -55,15 +82,64 @@ def signup(
         environ: WSGIEnvironment,
         start_response: StartResponse
 ) -> Iterable[bytes]:
+    """
+        Signup route
+    """
 
     params = get_params(environ) # get params from post request
     username, password = params["username"], params["password"]
 
+    # check if username exists already
+    with get_db() as conn:
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute(
+            "SELECT username FROM users WHERE username = %s",
+            (username,)
+        )
+        existing_users = cursor.fetchall()
+        cursor.close() # close the cursor
+
+    # if the username already exists
+    if existing_users:
+        data = {"message": f"Username {username} already exists"}
+        response_body: bytes = json.dumps(data).encode("utf-8")
+        start_response("409 Conflict", [("Content-Type", "application/json")])
+        return [response_body]
+
+    # otherwise, create a new user
+
+    user_id = str(uuid.uuid4())
+    hashed = hash_password(password) # hash the password
+
+    with get_db() as conn:
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute(
+            """
+                INSERT INTO users (id, username, password_hash)
+                VALUES (%s, %s, %s)
+            """,
+            (user_id, username, hashed)
+        )
+        conn.commit() # push changes
+        cursor.close() # close the cursor
     
-    data = {"message": f"Hello from signup! Username: {username}, Password: {password}"}
+    return redirect(start_response, "/")
+
+
+def login(
+        environ: WSGIEnvironment,
+        start_response: StartResponse
+) -> Iterable[bytes]:
+    """
+        Login route
+    """
+
+    params = get_params(environ)
+    username, password = params["username"], params["password"]
+
+    data = {"message": f"Hello from /login, username: {username}, password: {password}"}
     response_body: bytes = json.dumps(data).encode("utf-8")
     start_response("200 OK", [("Content-Type", "application/json")])
     return [response_body]
-
 
 
