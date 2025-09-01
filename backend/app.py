50be25d2f95b87e0b8a6350ebc9563963041b412
BlueTot <bluetotkc@gmail.com>
2025-09-01 11:17:34 +0100

    fix: fixed path error after refactor

--- backend/app.py
+++ backend/app.py
@@ -20,13 +20,13 @@ def application(
 
     match path:
         case "/":
-            return serve_html("../frontend/index.html", start_response)
+            return serve_html("../../frontend/index.html", start_response)
         case "/signup":
-            return serve_html("../frontend/signup.html", start_response)
+            return serve_html("../../frontend/signup.html", start_response)
         case "/login":
-            return serve_html("../frontend/login.html", start_response)
+            return serve_html("../../frontend/login.html", start_response)
         case "/dashboard":
-            return serve_html("../frontend/dashboard.html", start_response)
+            return serve_html("../../frontend/dashboard.html", start_response)
         case "/api/signup":
             return signup(environ, start_response)
         case "/api/login":
